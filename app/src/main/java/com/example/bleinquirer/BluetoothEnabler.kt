9fb256de5c49ab0f7fc223e40c178ff0b9c3a714
alkor97 <alkor@konto.pl>
2019-12-04 14:11:47 +0100

    Permission requesting and Bluetooth enabling moved to dedicated classes.

---
+++ app/src/main/java/com/example/bleinquirer/BluetoothEnabler.kt
@@ -0,0 +1,59 @@
+package com.example.bleinquirer
+
+import android.app.Activity
+import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothManager
+import android.content.Context
+import android.content.Intent
+import android.util.Log
+import androidx.annotation.UiThread
+import androidx.annotation.WorkerThread
+import java.util.concurrent.locks.ReentrantLock
+import kotlin.concurrent.withLock
+
+class BluetoothEnabler(private val activity: Activity) {
+
+    companion object {
+        const val REQUEST_ENABLE_BT = 317
+    }
+
+    private val lock = ReentrantLock()
+    private val condition = lock.newCondition()
+
+    private fun await() = lock.withLock { condition.await() }
+    private fun signal() = lock.withLock { condition.signalAll() }
+
+    private val bluetoothAdapter: BluetoothAdapter? by lazy(LazyThreadSafetyMode.NONE) {
+        val bluetoothManager =
+            activity.getSystemService(Context.BLUETOOTH_SERVICE) as BluetoothManager
+        bluetoothManager.adapter
+    }
+
+    @WorkerThread
+    fun ensureBluetoothIsEnabled(): Boolean {
+        if (bluetoothAdapter != null) {
+            bluetoothAdapter?.apply {
+                if (!isEnabled) {
+                    val enableBtIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
+                    activity.startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT)
+                    Log.d("ble", "Bluetooth enabling requested")
+                    await()
+                }
+            }
+        }
+        val enabled = bluetoothAdapter?.isEnabled ?: false
+        Log.d("ble", "Bluetooth is %sabled".format(if (enabled) "en" else "dis"))
+        return enabled
+    }
+
+    @UiThread
+    fun onActivityResult(
+        requestCode: Int,
+        resultCode: Int, @Suppress("UNUSED_PARAMETER") data: Intent?
+    ) {
+        if (requestCode == REQUEST_ENABLE_BT && resultCode != Activity.RESULT_OK) {
+            Log.i("ble", "user rejected request of enabling Bluetooth")
+        }
+        signal()
+    }
+}
--- app/src/main/java/com/example/bleinquirer/MainActivity.kt
+++ app/src/main/java/com/example/bleinquirer/MainActivity.kt
@@ -1,14 +1,6 @@
 package com.example.bleinquirer
 
-import android.Manifest
-import android.app.Activity
-import android.app.AlertDialog
-import android.bluetooth.BluetoothAdapter
-import android.bluetooth.BluetoothManager
-import android.content.Context
 import android.content.Intent
-import android.content.pm.PackageManager
-import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import android.view.View
@@ -18,46 +10,35 @@ import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
 
-    companion object {
-        const val REQUEST_ENABLE_BT = 1
-        const val REQUEST_COARSE_LOCATION_PERMISSION = 2
-    }
-
-    private val bluetoothAdapter: BluetoothAdapter? by lazy(LazyThreadSafetyMode.NONE) {
-        val bluetoothManager = getSystemService(Context.BLUETOOTH_SERVICE) as BluetoothManager
-        bluetoothManager.adapter
-    }
-
     private val appContext: BtLeApplication by lazy {
         applicationContext as BtLeApplication
+    }
+
+    private val permissionsRequester: PermissionRequester by lazy {
+        PermissionRequester(this)
+    }
+
+    private val bluetoothEnabler: BluetoothEnabler by lazy {
+        BluetoothEnabler(this)
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            if (checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-                val builder = AlertDialog.Builder(this)
-                builder.apply {
-                    setTitle("This app needs location update")
-                    setMessage("Please grant location update so this app can detect beacons.")
-                    setPositiveButton(android.R.string.ok, null)
-                    setOnDismissListener {
-                        requestPermissions(
-                            arrayOf(Manifest.permission.ACCESS_COARSE_LOCATION),
-                            REQUEST_COARSE_LOCATION_PERMISSION
-                        )
-                    }
-                }.show()
-            } else {
-                ensureBluetoothIsEnabled()
+        GlobalScope.launch {
+            try {
+                if (permissionsRequester.requestPermissions()) {
+                    bluetoothEnabler.ensureBluetoothIsEnabled()
+                }
+            } catch (e: Throwable) {
+                Log.e("permissions", "requesting permissions failed", e)
             }
-        } else {
-            ensureBluetoothIsEnabled()
         }
 
         val rv = rv_device_list as RecyclerView
@@ -87,7 +68,8 @@ class MainActivity : AppCompatActivity() {
         })
     }
 
-    private class DevicesObserver(val adapter: BtLeDevicesAdapter) : Observer<List<BtLeDeviceModel>> {
+    private class DevicesObserver(val adapter: BtLeDevicesAdapter) :
+        Observer<List<BtLeDeviceModel>> {
         override fun onChanged(newItems: List<BtLeDeviceModel>) {
             val oldItems = adapter.getItems()
             val result = DiffUtil.calculateDiff(DevicesDiffCallback(oldItems, newItems))
@@ -96,11 +78,17 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private class DevicesDiffCallback(val oldList: List<BtLeDeviceModel>, val newList: List<BtLeDeviceModel>) : DiffUtil.Callback() {
-        override fun areItemsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean = areContentsTheSame(oldItemPosition, newItemPosition)
+    private class DevicesDiffCallback(
+        val oldList: List<BtLeDeviceModel>,
+        val newList: List<BtLeDeviceModel>
+    ) : DiffUtil.Callback() {
+        override fun areItemsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean =
+            areContentsTheSame(oldItemPosition, newItemPosition)
+
         override fun getOldListSize(): Int = oldList.size
         override fun getNewListSize(): Int = newList.size
-        override fun areContentsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean = oldList[oldItemPosition] == newList[newItemPosition]
+        override fun areContentsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean =
+            oldList[oldItemPosition] == newList[newItemPosition]
     }
 
     override fun onRequestPermissionsResult(
@@ -108,39 +96,11 @@ class MainActivity : AppCompatActivity() {
         permissions: Array<String>,
         grantResults: IntArray
     ) {
-        if (requestCode == REQUEST_COARSE_LOCATION_PERMISSION) {
-            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                ensureBluetoothIsEnabled()
-            } else {
-                val builder = AlertDialog.Builder(this)
-                builder.apply {
-                    setTitle("Functionality limited")
-                    setMessage("Since location update has not been granted, this app will not be able to discover beacons when in the background.")
-                    setPositiveButton(android.R.string.ok, null)
-                    setOnDismissListener { }
-                }.show()
-            }
-        }
-    }
-
-    private fun ensureBluetoothIsEnabled() {
-        if (bluetoothAdapter != null) {
-            bluetoothAdapter?.apply {
-                if (!isEnabled) {
-                    val enableBtIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
-                    startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT)
-                    Log.d("ble", "Bluetooth enabling requested")
-                }
-            }
-        } else {
-            Log.e("ble", "Bluetooth not available")
-        }
+        permissionsRequester.onRequestPermissionsResult(requestCode, permissions, grantResults)
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        if (requestCode == REQUEST_ENABLE_BT && resultCode != Activity.RESULT_OK) {
-            Log.i("ble", "user rejected request of enabling Bluetooth")
-        }
+        bluetoothEnabler.onActivityResult(requestCode, resultCode, data)
         super.onActivityResult(requestCode, resultCode, data)
     }
 
